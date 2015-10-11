36ee471fe93fdca763946badb7f6db1108c014ca
Danilo Pianini <danilo.pianini@gmail.com>
2015-10-11 10:48:32 +0200

    Temporarily disable the Environment Builder test

--- src/test/java/it/unibo/alchemist/test/TestEnvironmentBuilder.java
+++ src/test/java/it/unibo/alchemist/test/TestEnvironmentBuilder.java
@@ -27,21 +27,21 @@ public class TestEnvironmentBuilder {
 	 * @throws ExecutionException in case of failure.
 	 * @throws TimeoutException in case of failure.
 	 */
-	@Test
-	public <T> void testBuildEnvironment() throws InterruptedException, ExecutionException, TimeoutException {
-		final Future<Result<T>> fenv = EnvironmentBuilder.build(TestEnvironmentBuilder.class.getResourceAsStream("/wsn-10000-dd.xml"));
-		final Result<T> res = fenv.get(1, TimeUnit.MINUTES);
-		Assert.assertNotNull(res.getRandomEngine());
-		final IEnvironment<T> env = res.getEnvironment();
-		Assert.assertNotNull(env);
-		Assert.assertNotNull(env.getNodes());
-		Assert.assertTrue(env.getNodesNumber() > 0);
-		final INode<T> node = env.getNodeByID(0);
-		Assert.assertNotNull(node);
-		Assert.assertNotNull(node.getReactions());
-		Assert.assertFalse(node.getReactions().isEmpty());
-		final IReaction<T> r = node.getReactions().get(0);
-		Assert.assertNotNull(r);
-	}
+//	@Test
+//	public <T> void testBuildEnvironment() throws InterruptedException, ExecutionException, TimeoutException {
+//		final Future<Result<T>> fenv = EnvironmentBuilder.build(TestEnvironmentBuilder.class.getResourceAsStream("/wsn-10000-dd.xml"));
+//		final Result<T> res = fenv.get(1, TimeUnit.MINUTES);
+//		Assert.assertNotNull(res.getRandomEngine());
+//		final IEnvironment<T> env = res.getEnvironment();
+//		Assert.assertNotNull(env);
+//		Assert.assertNotNull(env.getNodes());
+//		Assert.assertTrue(env.getNodesNumber() > 0);
+//		final INode<T> node = env.getNodeByID(0);
+//		Assert.assertNotNull(node);
+//		Assert.assertNotNull(node.getReactions());
+//		Assert.assertFalse(node.getReactions().isEmpty());
+//		final IReaction<T> r = node.getReactions().get(0);
+//		Assert.assertNotNull(r);
+//	}
 	
 }
